c29679902b80f09ce5d1be1cd1868a9812dec479
tolya <tolya.maximum@gmail.com>
2021-07-22 14:56:10 +0700

    move to html form attributes

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,6 @@ from authentication import forms
 
 urlpatterns = [
     path('signup', views.SignUpView.as_view(), name='signup'),
-    path('login', auth_views.LoginView.as_view(form_class=forms.LoginForm), name='login'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
