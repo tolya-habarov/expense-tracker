38e88a31ec5c4600bcd5e31182bd79fc60df6acc
tolya <tolya.maximum@gmail.com>
2021-08-05 17:58:23 +0700

    fix home url

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.urls.base import reverse_lazy
+from django.views.generic.base import RedirectView
 
 from app import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('transactions')), name='home'),
+
     path('transactions/', views.CurrentDateTransactionsView.as_view(), name='transactions'),
     path('transactions/<int:year>/<int:month>/', views.TransactionsView.as_view(), name='month_transactions'),
     path('transactions/add/', views.AddTransactionView.as_view(), name='add_transaction'),
